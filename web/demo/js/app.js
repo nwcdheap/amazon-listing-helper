52f05786f98961de1c71b79df3bdbeab52a12e83
dikers <dikers@gmail.com>
2021-01-11 13:25:41 +0800

    add

--- web/demo/js/app.js
+++ web/demo/js/app.js
@@ -4,7 +4,16 @@ $(function(){
             el: '#mainDiv',
             data:{
                 pictures: []
+             },
+             methods:{
+                 start_search:function(){   // 获取文件 文件保存在S3中
+                     start_search_inner()
+                 },
+                 show_search_result:function(){   // 获取文件 文件保存在S3中
+                      show_search_result_inner()
+                 },
              }
+
     })
     getData()
     initChart('chart01', '收入', '商品价格+运费', 5, 6, '#E0DEDE')
@@ -12,6 +21,7 @@ $(function(){
     initChart('chart03', '卖方收益', '收入-亚马逊销售费用-配送成本-仓储成本', 9, 11, '#E0DEDE')
     initChart('chart04', '净利', '收入-费用', 10, 21, '#F1F4F5')
 
+//    start_search_inner()
 });
 
 function getData(){
@@ -34,5 +44,17 @@ function getData(){
 //                  console.log(data.result);
 //              }
 //     })
+
+}
+
+function start_search_inner(){
+    $('#progressBar').modal('show')
+    setTimeout("show_search_result_inner()",2000)
+}
+
+
+function show_search_result_inner(){
+    $('#progressBar').modal('hide')
+    $('#staticBackdrop').modal('show')
 
 }
