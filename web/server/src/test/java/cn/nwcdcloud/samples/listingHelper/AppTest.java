e762533d56525ce33e93e2e9317fb82b48f2bc69
dikers <dikers@gmail.com>
2021-01-15 15:38:03 +0800

    爬虫

--- web/server/src/test/java/cn/nwcdcloud/samples/listingHelper/AppTest.java
+++ web/server/src/test/java/cn/nwcdcloud/samples/listingHelper/AppTest.java
@@ -3,6 +3,15 @@ package cn.nwcdcloud.samples.listingHelper;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
@@ -34,5 +43,67 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+
+    @org.junit.Test
+    public void testJSoup(){
+
+        try {
+
+
+            //创建HttpClient
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+
+            //目标网址
+            String url = "https://www.amazon.com/gp/product/B082PMS7TB/ref=silver_xx_cont_revecalc";
+
+            //创建请求方法
+            HttpGet httpGet = new HttpGet(url);
+
+            //设置Header模拟浏览器行为
+            httpGet.setHeader("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+
+            try {
+                //发送请求，收取响应
+                CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+
+                if(httpResponse.getStatusLine().getStatusCode() == 200){
+                    //解析响应
+                    String entity = EntityUtils.toString(httpResponse.getEntity());
+//                    System.out.println(entity);
+
+                    Document document = Jsoup.parse(entity, "utf-8");
+//                    Document document = Jsoup.connect("https://www.amazon.com/gp/product/B082PMS7TB/ref=silver_xx_cont_revecalc").get();
+                    System.out.println( "价格 ： " +  document.getElementById("price_inside_buybox"));
+                    System.out.println( "review ： " +  document.getElementById("acrCustomerReviewText"));
+
+                }
+
+                EntityUtils.consume(httpResponse.getEntity());
+                httpResponse.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+
+//            Document document = Jsoup.parse(new File("d://1.html"), "utf-8");
+            Document document = Jsoup.connect("https://www.amazon.com/gp/product/B082PMS7TB/ref=silver_xx_cont_revecalc").get();
+//            System.out.println(document);
+//            System.out.println( "价格 ： " +  document.getElementById("price_inside_buybox"));
+
+
+//            System.out.println( "review ： " +  document.getElementById("acrCustomerReviewText"));
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
